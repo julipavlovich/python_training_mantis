c00867332fa3e4f447d2bad1f46e7d83f7687419
Yuliya Pavlovich <juli.pavlovicvh@gmail.com>
2020-02-17 00:10:48 +0300

    Исправление 26

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,5 +1,6 @@
 from suds.client import Client
 from suds import WebFault
+from model.project import Project
 
 
 class SoapHelper:
@@ -10,6 +11,10 @@ class SoapHelper:
     def get_project_list(self, username, password):
         client = Client(self.app.base_url + "/api/soap/mantisconnect.php" + "?wsdl")
         try:
-            return client.service.mc_projects_get_user_accessible(username, password)
+            list = []
+            result=client.service.mc_projects_get_user_accessible(username, password)
+            for project in result:
+                project = Project(id=project.id, name=str(project.name), status=project.status, enabled=project.enabled, view_status=project.view_status, description=project.description)
+                return list.append(project)
         except WebFault:
             return ('SOAP Error')
